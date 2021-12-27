4b5ce1c192d92d46181fb9c0d42fccf8114f15c7
MichellDib <michell.dib28@gmail.com>
2021-12-27 16:37:29 +0100

    Task 3.2

--- Task 3/queries.sql
+++ Task 3/queries.sql
@@ -4,6 +4,19 @@ SELECT EXTRACT(month FROM time_start) AS month, COUNT(*) as lessons,
 COUNT(CASE type_of_lesson WHEN 'Individual' THEN 1 END) as individual,
 COUNT(CASE type_of_lesson WHEN 'Group' THEN 1 END) as group,
 COUNT(CASE type_of_lesson WHEN 'Ensamble' THEN 1 END) as ensamble
+
 FROM lesson 
 WHERE EXTRACT(year FROM time_start) = 2022
 GROUP BY EXTRACT(month FROM time_start);
+-- "The same as above, but retrieve the average number of lessons per month during the entire year, instead of the total 
+--  for each month."
+SELECT type_of_lesson as LessonType, COUNT(type_of_lesson) as numberOfLessons
+FROM lesson
+WHERE EXTRACT(year FROM time_start) = 2022
+GROUP BY LessonType
+
+UNION ALL
+
+SELECT 'SUM' type_of_lesson, COUNT(type_of_lesson)/12.
+FROM lesson
+WHERE EXTRACT(year FROM time_start) = 2022

043f134045c0459ceb8c7c5b377023ec90a43802
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-01-07 19:45:22 +0100

    commit

--- Task 2/lesson.sql
+++ Task 2/lesson.sql
@@ -1,40 +1,38 @@
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(2, 'Individual', '1','1', 'guzheng', 'advanced', '304', '500','classical','2021-12-27 13:00:00', '2021-12-27 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(3, 'Group', '3','10', 'guitar', 'beginner', '305', '100','western','2021-12-28 08:00:00', '2021-12-28 10:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(4, 'Group', '3','10', 'drums', 'intermediate', '301', '200','rock','2022-01-14 13:00:00', '2022-01-14 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(5, 'Individual', '1','1', 'piano', 'advanced', '302', '500','classical','2022-02-01 10:00:00', '2022-02-01 12:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(5, 'Individual', '1','1', 'piano', 'advanced', '302', '500','classical','2022-02-15 13:00:00', '2022-02-15 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(6, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2022-03-25 13:00:00', '2022-03-25 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(6, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2022-03-26 13:00:00', '2022-03-26 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(6, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2022-03-27 13:00:00', '2022-03-27 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(6, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2022-03-28 13:00:00', '2022-03-28 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(7, 'Individual', '1','1', 'guzheng', 'advanced', '304', '500','classical','2022-04-15 13:00:00', '2022-04-15 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(8, 'Individual', '1','1', 'piano', 'advanced', '304', '500','classical','2022-05-15 13:00:00', '2022-05-15 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(9, 'Group', '2','10', 'trumpet', 'beginner', '304', '100','jazz','2022-06-10 13:00:00', '2022-06-10 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(10, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2021-07-27 13:00:00', '2021-07-27 15:00:00');
-
-
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(2, 'Group', '5','10', 'kazoo', 'intermediate', '304', '100','punk','2021-01-30 13:00:00', '2021-07-27 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(3, 'Individual', '5','10', 'harp', 'beginner', '304', '100','classical','2021-01-20 13:00:00', '2021-07-27 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(4, 'Ensamble', '5','10', 'harp', 'beginner', '304', '100','classical','2021-01-12 13:00:00', '2021-07-27 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(5, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2021-01-15 13:00:00', '2021-07-27 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(3, 'Individual', '5','10', 'harp', 'beginner', '304', '100','classical','2021-01-15 13:00:00', '2021-07-27 15:00:00');
-INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end) 
-VALUES(1, 'Ensamble', '5','10', 'harp', 'beginner', '304', '100','classical','2021-01-15 13:00:00', '2021-07-27 15:00:00');
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(2, 'Individual', '1','1', 'guzheng', 'advanced', '304', '500','classical','2021-12-27 13:00:00', '2021-12-27 15:00:00', 1);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(3, 'Group', '3','10', 'guitar', 'beginner', '305', '100','western','2021-12-28 08:00:00', '2021-12-28 10:00:00', 3);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(4, 'Group', '3','10', 'drums', 'intermediate', '301', '200','rock','2022-01-14 13:00:00', '2022-01-14 15:00:00', 4);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(5, 'Individual', '1','1', 'piano', 'advanced', '302', '500','classical','2022-02-01 10:00:00', '2022-02-01 12:00:00', 1);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(5, 'Individual', '1','1', 'piano', 'advanced', '302', '500','classical','2022-02-15 13:00:00', '2022-02-15 15:00:00', 1);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(6, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2022-03-25 13:00:00', '2022-03-25 15:00:00', 8);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(6, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2022-03-26 13:00:00', '2022-03-26 15:00:00', 9);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(6, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2022-03-27 13:00:00', '2022-03-27 15:00:00', 10);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(6, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2022-03-28 13:00:00', '2022-03-28 15:00:00', 1);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(7, 'Individual', '1','1', 'guzheng', 'advanced', '304', '500','classical','2022-04-15 13:00:00', '2022-04-15 15:00:00', 1);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(8, 'Individual', '1','1', 'piano', 'advanced', '304', '500','classical','2022-05-15 13:00:00', '2022-05-15 15:00:00', 1);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(9, 'Group', '2','10', 'trumpet', 'beginner', '304', '100','jazz','2022-06-10 13:00:00', '2022-06-10 15:00:00', 5);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(10, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2021-07-27 13:00:00', '2021-07-27 15:00:00', 5);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(2, 'Group', '5','10', 'kazoo', 'intermediate', '304', '100','punk','2021-01-30 13:00:00', '2021-07-27 15:00:00', 5);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(3, 'Individual', '5','10', 'harp', 'beginner', '304', '100','classical','2021-01-20 13:00:00', '2021-07-27 15:00:00', 1);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(4, 'Ensamble', '5','10', 'harp', 'beginner', '304', '100','classical','2021-01-12 13:00:00', '2021-07-27 15:00:00', 3);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(5, 'Group', '5','10', 'harp', 'beginner', '304', '100','classical','2021-01-15 13:00:00', '2021-07-27 15:00:00', 2);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(3, 'Individual', '5','10', 'harp', 'beginner', '304', '100','classical','2021-01-15 13:00:00', '2021-07-27 15:00:00', 1);
+INSERT INTO lesson(instructor_id, type_of_lesson, min_nr_slots, max_nr_slots, instrument, skill_level, place, price, genre, time_start, time_end, amount_of_students) 
+VALUES(1, 'Ensamble', '5','10', 'harp', 'beginner', '304', '100','classical','2021-01-15 13:00:00', '2021-07-27 15:00:00', 10);
--- Task 3/queries.sql
+++ Task 3/queries.sql
@@ -1,6 +1,6 @@
 -- "Show the number of lessons given per month during a specified year. It shall be possible to retrieve the total number of 
 -- lessons per month (just one number per month) and the specific number of individual lessons, group lessons and ensembles (three numbers per month)."
-SELECT EXTRACT(month FROM time_start) AS month, COUNT(*) as lessons, 
+CREATE VIEW view1 AS SELECT EXTRACT(month FROM time_start) AS month, COUNT(*) as lessons, 
 COUNT(CASE type_of_lesson WHEN 'Individual' THEN 1 END) as individual,
 COUNT(CASE type_of_lesson WHEN 'Group' THEN 1 END) as group,
 COUNT(CASE type_of_lesson WHEN 'Ensamble' THEN 1 END) as ensamble
@@ -8,9 +8,10 @@ COUNT(CASE type_of_lesson WHEN 'Ensamble' THEN 1 END) as ensamble
 FROM lesson 
 WHERE EXTRACT(year FROM time_start) = 2022
 GROUP BY EXTRACT(month FROM time_start);
+
 -- "The same as above, but retrieve the average number of lessons per month during the entire year, instead of the total 
 --  for each month."
-SELECT type_of_lesson as LessonType, COUNT(type_of_lesson) as numberOfLessons
+CREATE VIEW view2 AS SELECT type_of_lesson as LessonType, COUNT(type_of_lesson) as numberOfLessons
 FROM lesson
 WHERE EXTRACT(year FROM time_start) = 2022
 GROUP BY LessonType
@@ -23,10 +24,26 @@ WHERE EXTRACT(year FROM time_start) = 2022
 
 --"List all instructors who has given more than a specific number of lessons during the current month. Sum all lessons, independent of type,
 --and sort the result by the number of given lessons. This query will be used to find instructors risking to work too much, and will be executed daily.""
-EXPLAIN ANALYZE CREATE MATERIALIZED VIEW mv1 AS
+EXPLAIN ANALYZE CREATE VIEW view3 AS
 SELECT instructor_id as Instructor,
 COUNT(type_of_lesson) as TotalLessons
 FROM lesson
 
 WHERE EXTRACT(month FROM time_start) = 1
 GROUP BY Instructor HAVING COUNT(type_of_lesson)>1;
+
+--List all ensembles held during the next week, sorted by music genre and weekday. For each ensemble tell whether it's full booked, has 1-2 seats left 
+--or has more seats left. Hint: you might want to use a CASE statement in your query to produce the desired output.
+CREATE MATERIALIZED VIEW mv AS SELECT
+to_char(time_start, 'Day') AS Weekday,
+genre AS Genre,
+max_nr_slots AS Seats,
+amount_of_students AS Amount,
+CASE
+	WHEN amount_of_students = CAST(max_nr_slots AS INTEGER) THEN 'Full booked'
+	WHEN amount_of_students > CAST(max_nr_slots AS INTEGER) - 3 THEN 'One or two seats left'
+	ELSE 'Many seats left'
+END AS Booked
+FROM lesson
+WHERE type_of_lesson='Ensamble' AND EXTRACT(week from time_start) = EXTRACT(week from now()) + 1
+GROUP BY Weekday, Genre, Seats, Booked, Amount;
